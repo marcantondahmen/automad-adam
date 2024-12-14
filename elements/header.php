81d6d24f139fc978f53062d48934a1d710952ded
Marc Anton Dahmen <mail@marcdahmen.de>
2024-12-14 10:42:53 +0100

    fix meta tags

--- elements/header.php
+++ elements/header.php
@@ -7,8 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
 		<title>@{ metaTitle | def('@{ sitename } / @{ title | def ("404") }') }</title>
-		<@ elements/metatags.php @>
-		<@ elements/favicons.php @>
+		<@ metatags.php @>
+		<@ favicons.php @>
 	<?php } ?>
 	<# 
 	

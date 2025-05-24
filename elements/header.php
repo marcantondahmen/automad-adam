19fe3eb6c6564ecb07e1268b6c97342befe65fa5
Marc Anton Dahmen <mail@marcdahmen.de>
2025-05-24 12:03:12 +0200

    feat: use new meta tags engine in Automad 2

--- elements/header.php
+++ elements/header.php
@@ -2,13 +2,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<@ set { :version: '<?php echo AM_VERSION; ?>' } @>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
-		<title>@{ metaTitle | def('@{ sitename } / @{ title | def ("404") }') }</title>
+		<@ metatags.php @>
 	<?php } ?>
-	<@ metatags.php @>
 	<@ favicons.php @>
 	<# 
 	
@@ -20,8 +18,8 @@
 	#>
 	<link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800&display=swap" rel="stylesheet">	
-	<link href="/packages/@{ theme }/dist/adam.min.css?v=@{ :version | sanitize }" rel="stylesheet">
-	<script src="/packages/@{ theme }/dist/adam.min.js?v=@{ :version | sanitize }"></script>
+	<link href="/packages/@{ theme }/dist/adam.min.css" rel="stylesheet">
+	<script src="/packages/@{ theme }/dist/adam.min.js"></script>
 	<# Add optional header items. #>
 	@{ itemsHeader }
 </head>
--- elements/metatags.php
+++ elements/metatags.php
@@ -1,5 +1,5 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
-<?php if (substr(AM_VERSION, 0, 1) == '1') { ?>
+<title>@{ metaTitle | def('@{ sitename } / @{ title | def ("404") }') }</title>
 <@ set_teaser_variable.php @>
 <@ set { :description: @{ metaDescription | def(@{ :teaser | stripTags }) } } @>
 <@ Automad/MetaTags { 
@@ -13,6 +13,3 @@
 			def('*.jpg, *.png, *.gif, /shared/*.jpg, /shared/*.png, /shared/*.gif') 
 		}
 } @>
-<?php } else { ?>
-	<@ Automad/MetaTags { ogTitle: false, ogImage: @{ ogImage } } @>
-<?php } ?>
